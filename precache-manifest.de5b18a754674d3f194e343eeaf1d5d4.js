3ea559c5c854908f47bde2d5a8a0ab3b076eedb0
ermogenes <ermogenes@users.noreply.github.com>
2021-11-19 13:39:25 +0000

    Deploying to gh-pages from @ ermogenes/trivia-experiment@c30355a3db1bd06c1c991dc1ee53d654576a7c09 🚀

---
+++ precache-manifest.de5b18a754674d3f194e343eeaf1d5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d14232e9751e26e7ef0",
+    "url": "/trivia-experiment/css/app.853ac122.css"
+  },
+  {
+    "revision": "ffb3107260b6a98db00cc9e6457416b8",
+    "url": "/trivia-experiment/index.html"
+  },
+  {
+    "revision": "7d14232e9751e26e7ef0",
+    "url": "/trivia-experiment/js/app.8d15ca91.js"
+  },
+  {
+    "revision": "dc7c224a8246871dab75",
+    "url": "/trivia-experiment/js/chunk-vendors.1e5c8c70.js"
+  },
+  {
+    "revision": "32baf8e7d26597ebcd4061806408943e",
+    "url": "/trivia-experiment/manifest.json"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/trivia-experiment/robots.txt"
+  }
+]);
